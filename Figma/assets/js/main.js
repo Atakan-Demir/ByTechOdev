e863ae9542a422f10d681fa376798204de8b8b14
Atakan Demir <101272960+Atakan-Demir@users.noreply.github.com>
2024-03-25 06:45:09 +0300

    Atakan Demir
    +Navbar Section (-Mobile)
    +Hero Section (-Mobile)
    +Best Section (-Mobile)
    +Join Section (-Mobile)
    +Testimonial Section (-Mobile)
    +Collection Section (-Mobile)
    +Statistic Section (-Mobile)
    +Footer Section (-Mobile)

---
+++ Figma/assets/js/main.js
@@ -0,0 +1,111 @@
+
+window.addEventListener('load', function () {
+
+    slider('.swiper');
+
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var nextButton = document.getElementById('next-slide');
+    var swiperNextButton = document.querySelector('.swiper-button-next');
+    var btnCollections = document.querySelectorAll('.btn-collection');
+
+    nextButton.addEventListener('click', function () {
+        swiperNextButton.click();
+    });
+
+    var prevButton = document.getElementById('prev-slide');
+    var swiperPrevButton = document.querySelector('.swiper-button-prev');
+
+    prevButton.addEventListener('click', function () {
+        swiperPrevButton.click();
+    });
+
+    
+    btnCollections.forEach(function (btnCollection) {
+       
+        btnCollection.addEventListener('click', function () {
+            hideArros(btnCollections);
+            showArrow(btnCollection);
+        });
+    });
+
+    statisticCounter();
+
+
+});
+
+/* Slider crate*/
+function slider(element, options) {
+    if (typeof options === 'undefined') {
+        options = {};
+    }
+    new Swiper(element, {
+        slidesPerView: options.slidesPerView ? options.slidesPerView : 4,
+        spaceBetween: options.spaceBetween ? options.spaceBetween : 30,
+        centeredSlides: options.centeredSlides ? options.centeredSlides : true,
+        loop: options.loop ? options.loop : true,
+        height: options.height ? options.height : 100,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        }
+    })
+}
+
+/* Show Arrow in collectionb button */
+function showArrow(btnCollection) {
+    btnCollection.querySelector('img:last-child').classList.remove('arrow-off');
+    btnCollection.querySelector('img:last-child').classList.add('arrow-on');
+}
+
+/* Hide Arrow in collections button */
+function hideArros(btnCollections) {
+    
+    btnCollections.forEach(function (btnCollection) {
+
+        var img = btnCollection.querySelector('img:last-child');
+        if(img.classList.contains('arrow-on')){
+            img.classList.remove('arrow-on');
+            img.classList.add('arrow-off');
+        }
+ 
+    });
+}
+
+
+/* gorus alanina girince say*/
+function statisticCounter(){
+    var startNumber = 0;
+          var endNumber = 11658467; // sayi
+          var duration = 3000; // sure
+          var numberElement = document.getElementById('number');
+        
+          var startTime;
+          function animateNumber(timestamp) {
+            if (!startTime) startTime = timestamp;
+            var progress = timestamp - startTime;
+            var percentage = Math.min(progress / duration, 1);
+            var currentNumber = Math.floor(startNumber + (endNumber - startNumber) * percentage);
+            numberElement.textContent = currentNumber.toLocaleString('en-US'); // bu, noktayi virgul yapiyormus
+            if (progress < duration) {
+              requestAnimationFrame(animateNumber);
+            }
+          }
+        
+          // gorus alani
+          var statisticSection = document.querySelector('.statistic-container');
+          var observer = new IntersectionObserver(function(entries) {
+            entries.forEach(function(entry) {
+              if (entry.isIntersecting) {
+                requestAnimationFrame(animateNumber);
+                observer.unobserve(statisticSection); // gorus alanini durdur
+              }
+            });
+          });
+          observer.observe(statisticSection);
+}
